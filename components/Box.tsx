1e442a8abc7413126fc0d33f288e87b1dda41d5d
Ali <AliNazarii7394@gmail.com>
2023-10-05 18:21:41 +0330

    sidbar component defactor to Box component

---
+++ components/Box.tsx
@@ -0,0 +1,12 @@
+import { twMerge } from "tailwind-merge";
+
+interface boxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Box: React.FC<boxProps> = ({ children, className }) => {
+  return <div className={twMerge(`h-fit w-full`, className)}>{children}</div>;
+};
+
+export default Box;
