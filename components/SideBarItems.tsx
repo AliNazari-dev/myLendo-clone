fa66e4dcbebc34117eb310a601f5a76a004316e3
Ali <AliNazarii7394@gmail.com>
2023-10-05 18:21:05 +0330

    SidebarItem component added to sidebar

---
+++ components/SideBarItems.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+interface SidebarItemProps {
+  src: string;
+  label: string;
+  active?: boolean;
+  href: string;
+  className?: string;
+}
+const SideBarItems: React.FC<SidebarItemProps> = ({ src, label, active, href, className }) => {
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        `flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-blue-700 transition-colors text-neutral-800 py-1`,
+        active && "text-blue-500"
+      )}>
+      <div
+        className={` flex w-10 h-10 justify-center items-center rounded-xl ${
+          active ? "bg-blue-200" : "bg-gray-200"
+        }`}>
+        <Image alt='' src={src} width={20} height={20} />
+      </div>
+      <p className={`truncate w-full ${className}`}>{label}</p>
+    </Link>
+  );
+};
+
+export default SideBarItems;
